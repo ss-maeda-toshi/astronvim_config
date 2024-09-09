5bf4bf4ed54d3f4f2fc9f45cde14bf9b817dbfd5
Maeda Toshiaki <toshiaki.maeda@supership.jp>
2024-09-09 13:38:12 +0900

    lazydocker

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.docker.lazydocker" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-clock-statusline" },
