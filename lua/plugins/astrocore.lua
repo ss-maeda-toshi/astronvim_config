91fc543755cb7d0dd2bdff245053aa8e5b833982
Maeda Toshiaki <toshiaki.maeda@supership.jp>
2024-09-10 12:57:43 +0900

    fix c-h -> c-H

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -82,7 +82,7 @@ return {
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
-        ["<C-h>"] = { "<BS>" },
+        ["<C-H>"] = { "<BS>" },
       },
     },
   },
